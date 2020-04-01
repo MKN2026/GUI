c72ed9f94bd5f268faec66d7647ccd33435a5568
MKN2026 <mkn2026@gmail.com>
2020-04-02 00:48:12 +0400

    Nothing New

--- Basic_GUI.py
+++ Basic_GUI.py
@@ -11,4 +11,12 @@ canvas.pack()
 frame = tk.Frame(root, bg="#800000")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
+openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="white", bg="#80000" command="addApp")
+openFile.pack()
+
+runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="white", bg="#800000")
+runApps.pack()
+
+
+
 root.mainloop()
